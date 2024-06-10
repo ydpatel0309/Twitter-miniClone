3180d9e43fd78526d1ef36d898ca26450fc0cbe4
Yash Patel <91671051+ydpatel0309@users.noreply.github.com>
2024-06-10 13:16:32 +0530

    implemented inirial loader

--- client/src/routes/home.js
+++ client/src/routes/home.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import HomeBody from "../components/HomeBody";
-
+import "./Loader.css"; 
+import loaderImage from '../asset/x.jpg'; 
 function Home() {
+  const [loaderVisible, setLoaderVisible] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoaderVisible(false);
+    }, 1000); 
+
+    return () => clearTimeout(timer); 
+  }, []);
+
   return (
     <>
-    
+      <div className={`loader ${loaderVisible ? "" : "hidden"}`}>
+        <img src={loaderImage} alt="Loading" />
+      </div>
       <HomeBody />
     </>
   );
